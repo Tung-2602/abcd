2547bb488fab28a9c2b8b88568f0df50b21260fd
unknown <zeddtungvctb@gmail.com>
2023-05-12 19:59:09 +0700

    aaa

---
+++ assets/js/data.js
@@ -0,0 +1,19 @@
+const category={
+    {cat_id: 'phone', cat_name: 'Dien thoai'},
+    {cat_id: 'laptop', cat_name: 'May tinh'},
+    {cat_id: 'ram', cat_name: 'Bo nho trong'},
+};
+const product=[
+    {
+        id:1, name:'samsung-galaxy-a24', price:200, 
+        img:'https://chamsocdidong.com/images/service/2021/11/06/original/thay-cam-ung-iphone-13-pro_1636138530.jpg', cat_id: 'phone'
+    }
+    {
+        id:2, name:'iphone 2', price:2020, 
+        img:'https://chamsocdidong.com/images/service/2021/11/06/original/thay-cam-ung-iphone-13-pro_1636138530.jpg', cat_id: 'phone'
+    }
+    {
+        id:3, name:'iphone 2', price:20220, 
+        img:'https://chamsocdidong.com/images/service/2021/11/06/original/thay-cam-ung-iphone-13-pro_1636138530.jpg', cat_id: 'laptop'
+    }
+];
